09179262e433bbcb0d652562843a6a388dabe0a2
Jason-Bai <boybai1213@gmail.com>
2018-04-20 00:21:58 +0800

    添加开奖时间

--- webui/src/containers/Home/index.js
+++ webui/src/containers/Home/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import NodeSchedule from 'node-schedule';
+import moment from 'moment';
 import Helmet from '../../components/Helmet';
 import Table from '../../components/Table';
 import Number from '../../components/Number';
@@ -35,7 +36,7 @@ const renderNumber = (number) => {
     title: <Number number={number} />,
     dataIndex: `index${number}`,
     className: 'text-center column-number',
-    width: '8%',
+    width: '7%',
     render,
   };
 
@@ -51,17 +52,24 @@ class Home extends React.Component {
     ].map((number) => renderNumber(number));
 
     const beforeColumns =[{
-      title: '开奖期数',
+      title: '开奖时间',
       dataIndex: 'id',
       key: 'id',
-      width: '10%',
+      width: '12%',
+      render: (id, record) => <span>{moment(record.date).format('YYYYMMDDHHmmss')}</span>,
+    }, {
+      title: '开奖期数',
+      dataIndex: 'date',
+      key: 'date',
+      className: 'text-center',
+      width: '8%',
       render: (id, record) => {
         const { date } = record;
         const n = new Date(date);
         const number = n.getHours() * 60 + n.getMinutes();
         return (
           <span>
-          {number}
+          {number.toString().padStart(4, '0')}
           </span>
         );
       },
