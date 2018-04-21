7f482c7672f440af800900f6347a1072b10929d9
Jason-Bai <boybai1213@gmail.com>
2018-04-21 10:35:01 +0800

    4秒取数

--- services/cron.js
+++ services/cron.js
@@ -7,7 +7,7 @@ const schedule = require('node-schedule');
 const exec = util.promisify(childProcess.exec);
 
 /* eslint no-unused-vars: 0 */
-const tencentOnline = schedule.scheduleJob('5 */1 * * * *', async () => {
+const tencentOnline = schedule.scheduleJob('4 */1 * * * *', async () => {
   const cmdFile = './bin/tencent_im_online.js';
   const { error, stdout } = await exec(cmdFile);
   console.log(`Error: ${error}`);
