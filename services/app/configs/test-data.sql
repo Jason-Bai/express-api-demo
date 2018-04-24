83f2b29b2e8453987b7e8c9c2e85f2217e77904c
Jason-Bai <boybai1213@gmail.com>
2018-04-24 08:08:00 +0800

    测试通过

--- services/app/configs/test-data.sql
+++ services/app/configs/test-data.sql
@@ -1,7 +1,7 @@
 -- user
 INSERT INTO `user` (`id`, `name`, `email`, `password`, `salt`, `role`, `isDelete`, `createdAt`, `updatedAt`) VALUES (1, 'baiyu', '602316022@qq.com', '0296d58ee532f6dde7058453ff5205c1', 'YigRivsMpZYHXZis', 'admin', 'no', NOW(), NOW());
 -- imonline
-INSERT INTO `imonline` (`id`, `status`, `current`, `history`, `date`, `quarter`, `month`, `weekday`, `hour`, `minute`, `result`, `createdAt`, `updatedAt`)
+INSERT INTO `imonline` (`id`, `number`, `current`, `history`, `date`, `quarter`, `month`, `weekday`, `hour`, `minute`, `result`, `createdAt`, `updatedAt`)
 VALUES
-	(1, 'predicted', 273625519, 282877126, '2018-04-23 13:15:05', 2, 4, 1, 13, 15, '05519', '2018-04-23 13:15:05', '2018-04-23 13:15:05');
+	(1, '201804230795', 273625519, 282877126, '2018-04-23 13:15:05', 2, 4, 1, 13, 15, '05519', '2018-04-23 13:15:05', '2018-04-23 13:15:05');
 
